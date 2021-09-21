dac8e617f8db17b2d419ba19b8b39b200ea6556a
TengkuRafi <63381758+TengkuRafi@users.noreply.github.com>
2021-09-21 19:52:16 +0700

    Mengerjakan dan Mempelajari Class dan Object

---
+++ Pertemuan2/coba.php
@@ -0,0 +1,21 @@
+<?php
+/*
+Tengku Muhammad Rafi
+193040091
+https://github.com/TengkuRafi/prakweb2021_oophp_193040091
+Pertemuan2
+Mengerjakan dan Mempelajari Class & Object
+21 September 2021
+*/
+
+class Coba{
+
+}
+
+$a = new Coba();
+$b = new Coba();
+$c = new Coba();
+
+var_dump($a);
+var_dump($b);
+var_dump($c);
