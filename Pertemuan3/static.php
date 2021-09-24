b93cfad43c0dc21ba010624ab95c5ab5cc08c541
TengkuRafi <63381758+TengkuRafi@users.noreply.github.com>
2021-09-24 23:58:13 +0700

    Mengerjakan Vidio tentang Static Keyword Setter & Getter

---
+++ Pertemuan3/static.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+Tengku Muhammad Rafi
+193040091
+https://github.com/TengkuRafi/prakweb2021_oophp_193040091
+Pertemuan3
+Mengerjakan dan Mempelajari Static Keyword setter & getter
+24 September 2021
+*/
+
+//class ContohStatic {
+//    public static $angka = 1;
+
+//    public static function halo(){
+//        return "Halo." . self::$angka++. "kali.";
+//    }
+//}
+
+//echo ContohStatic :: $angka;
+//echo "<br>";
+//echo ContohStatic ::halo();
+//echo "<hr>";
+//echo ContohStatic:: halo();
+
+class Contoh {
+    public static $angka = 1;
+
+    public function halo(){
+        return "Halo " . self::$angka++."kali. <br>";
+    }
+}
+
+$obj = new Contoh;
+echo$obj->halo();
+echo$obj->halo();
+echo$obj->halo();
+
+echo "<hr>";
+
+$obj2 = new Contoh;
+echo$obj2->halo();
+echo$obj2->halo();
+echo$obj2->halo();
+
+
+?>
